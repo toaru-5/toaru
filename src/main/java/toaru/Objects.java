1944c9322948bc5aa1c94c2e9701c6408cd8bbf3
yusukemac <townstock@yahoo.co.jp>
2014-07-11 18:34:46 +0900

    初回コミット&Fix

---
+++ src/main/java/toaru/Objects.java
@@ -0,0 +1,24 @@
+package toaru;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class Objects {
+
+	public static Item learning_device, nitrogen, compressed_air;
+	public static CreativeTabs tabToaru = new CreativeTabs("Toaru") {
+		@Override
+		@SideOnly(Side.CLIENT) 
+		public Item getTabIconItem() {
+			return nitrogen;
+		}
+	};
+	
+	public static void setupObjects() {
+		learning_device = new Item().setCreativeTab(tabToaru).setUnlocalizedName("Lerningdevice").setTextureName("LearningDevice");
+		nitrogen = new Item().setCreativeTab(tabToaru).setUnlocalizedName("LiquidNitrogenCan").setTextureName("LiquidNitrogenCan");
+		compressed_air = new Item().setCreativeTab(tabToaru).setUnlocalizedName("CompressedAir").setTextureName("CompressedAir");
+	}
+}
---
+++ src/main/java/toaru/Toaru.java
@@ -0,0 +1,25 @@
+package toaru;
+
+import net.minecraft.item.Item;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+@Mod(modid = "Toaru", name = "Toaru", version = "1.0")
+public class Toaru {
+	
+	@EventHandler
+	public void Init(FMLInitializationEvent event) {
+		
+	}
+	@EventHandler
+	public void PreInit(FMLPreInitializationEvent event) {
+		Objects.setupObjects();
+		Item[] items = new Item[] {Objects.compressed_air, Objects.learning_device, Objects.nitrogen};
+		for (Item item : items) {
+			GameRegistry.registerItem(item, item.getUnlocalizedName());
+		}
+	}
+}
