3aad6f5ac0948f21438229fb9a569b3467854f3f
hukihamu <ksk19982626@yahoo.co.jp>
2014-07-13 10:39:23 +0900

    アイテム追加
    電極追加

--- src/main/java/toaru/Objects.java
+++ src/main/java/toaru/Objects.java
@@ -5,22 +5,27 @@ import net.minecraft.item.Item;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class Objects {
+public class Objects 
+{
 
-	public static Item learning_device, nitrogen, compressed_air, iron_nails, cyborg_hand;
-	public static CreativeTabs tabToaru = new CreativeTabs("Toaru") {
+	public static Item learning_device, nitrogen, compressed_air, iron_nails, cyborg_hand, Electrode;
+	public static CreativeTabs tabToaru = new CreativeTabs("Toaru") 
+	{
 		@Override
 		@SideOnly(Side.CLIENT) 
-		public Item getTabIconItem() {
+		public Item getTabIconItem() 
+		{
 			return learning_device;
 		}
 	};
 	
-	public static void setupObjects() {
+	public static void setupObjects() 
+	{
 		learning_device = new Item().setCreativeTab(tabToaru).setUnlocalizedName("LearningDevice").setTextureName("LearningDevice");
 		nitrogen = new Tools.LiquidNitrogenCan().setCreativeTab(tabToaru).setUnlocalizedName("LiquidNitrogenCan").setTextureName("LiquidNitrogenCan");
-		compressed_air = new Item().setCreativeTab(tabToaru).setUnlocalizedName("CompressedAir").setTextureName("CompressedAir");
+		compressed_air = new Item().setCreativeTab(tabToaru).setUnlocalizedName("CompressedAir").setTextureName("CompressedAir").setMaxStackSize(16);
 		iron_nails = new Item().setCreativeTab(tabToaru).setUnlocalizedName("IronNails").setTextureName("IronNails");
 		cyborg_hand = new Item().setCreativeTab(tabToaru).setUnlocalizedName("CyborgHand").setTextureName("CyborgHand");
+		Electrode = new Item().setCreativeTab(tabToaru).setUnlocalizedName("Electrode").setTextureName("Electrode");
 	}
 }
--- src/main/java/toaru/Toaru.java
+++ src/main/java/toaru/Toaru.java
@@ -8,17 +8,21 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = "Toaru", name = "Toaru", version = "1.0")
-public class Toaru {
+public class Toaru 
+{
 	
 	@EventHandler
-	public void Init(FMLInitializationEvent event) {
+	public void Init(FMLInitializationEvent event) 
+	{
 		
 	}
 	@EventHandler
-	public void PreInit(FMLPreInitializationEvent event) {
+	public void PreInit(FMLPreInitializationEvent event) 
+	{
 		Objects.setupObjects();
-		Item[] items = new Item[] {Objects.compressed_air, Objects.learning_device, Objects.nitrogen, Objects.iron_nails, Objects.cyborg_hand};
-		for (Item item : items) {
+		Item[] items = new Item[] {Objects.compressed_air, Objects.learning_device, Objects.nitrogen, Objects.iron_nails, Objects.cyborg_hand, Objects.Electrode};
+		for (Item item : items) 
+		{
 			GameRegistry.registerItem(item, item.getUnlocalizedName());
 		}
 	}
